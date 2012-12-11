a65d8f241aa493a249af61b133923c0ed57d9b89
Matthias Wessendorf <matzew@apache.org>
2012-12-11 15:30:39 +0100

    Removing unneeded dummy files and just using the implementation/method file for the tests...

--- AeroGear-StoreTests/AeroGear_StoreTests.m
+++ AeroGear-StoreTests/AeroGear_StoreTests.m
@@ -1,13 +1,26 @@
-//
-//  AeroGear_StoreTests.m
-//  AeroGear-StoreTests
-//
-//  Created by Matthias Wessendorf on 12/11/12.
-//  Copyright (c) 2012 Matthias Wessendorf. All rights reserved.
-//
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012 Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-#import "AeroGear_StoreTests.h"
+#import <SenTestingKit/SenTestingKit.h>
 
+@interface AeroGear_StoreTests : SenTestCase
+
+@end
 @implementation AeroGear_StoreTests
 
 - (void)setUp
@@ -26,7 +39,7 @@
 
 - (void)testExample
 {
-    STFail(@"Unit tests are not implemented yet in AeroGear-StoreTests");
+    //STFail(@"Unit tests are not implemented yet in AeroGear-StoreTests");
 }
 
 @end
